d424731c632ebd58305728728c37c3157e8cfe2a
Shubhankar Shyamal <shubhankar.shyamal@docquity.com>
2025-06-18 00:38:37 +0530

    initial commit

---
+++ src/main/java/com/selflearning/paryavekshak/ParyavekshakApplication.java
@@ -0,0 +1,13 @@
+package com.selflearning.paryavekshak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParyavekshakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParyavekshakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selflearning/paryavekshak/ParyavekshakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selflearning.paryavekshak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParyavekshakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
